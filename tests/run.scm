1c07e32991d129c67078c2821bba45da90d33e98
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-05-10 16:28:44 -0400

    Tests: Add shim for SRFI 64 test syntax.

--- tests/run.scm
+++ tests/run.scm
@@ -2,6 +2,18 @@
         test
         (srfi 232))
 
+;;; SRFI 64 -> test shim.
+
+(define-syntax test-eqv
+  (syntax-rules ()
+    ((test-eqv expected expr)
+     (test expected expr))))
+
+(define-syntax test-equal
+  (syntax-rules ()
+    ((test-equal expected expr)
+     (test expected expr))))
+
 (include "test-body.scm")
 
 (test-exit)
